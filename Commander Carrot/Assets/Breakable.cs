500c52e218aa139b5ddee6fe6ecee6ed362cf277
Jamblefoot <32472250+Jamblefoot@users.noreply.github.com>
2023-05-29 12:39:37 -0700

    workin on crates

---
+++ Commander Carrot/Assets/Breakable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    [SerializeField] GameObject brokenPrefab;
+    void OnParticleCollision(GameObject other)
+    {
+        
+    }
+}
