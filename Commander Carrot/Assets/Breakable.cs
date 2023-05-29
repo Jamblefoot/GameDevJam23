f1e404a92cd73c69a0f0445ffcd0f3e463ca0ac0
Jamblefoot <32472250+Jamblefoot@users.noreply.github.com>
2023-05-29 12:53:42 -0700

    crates break (?)

--- Commander Carrot/Assets/Breakable.cs
+++ Commander Carrot/Assets/Breakable.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 public class Breakable : MonoBehaviour
 {
     [SerializeField] GameObject brokenPrefab;
+    int health = 3;
     void OnParticleCollision(GameObject other)
     {
-        
+        Debug.Log("ENEMY HIT BY A PARTICLE FROM " + other.transform.root.gameObject.name);
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+        ParticleSystem part = other.GetComponent<ParticleSystem>();
+        part.GetCollisionEvents(gameObject, collisionEvents);
+        health -= collisionEvents.Count;
+        if (health <= 0)
+        {
+            Break();
+        }
+    }
+
+    void Break()
+    {
+        Destroy(Instantiate(brokenPrefab, transform.position, transform.rotation, transform.parent), 10f);
+        Destroy(gameObject);
     }
 }
